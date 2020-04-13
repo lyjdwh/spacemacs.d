741e5bffed04f6a6681059e153353b9dc33d2595
lyjdwh <1412511544@qq.com>
2020-04-13 23:42:20 +0800

    add doom theme

--- init.el
+++ init.el
@@ -140,7 +140,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(sicp ssh-agency anki-editor company-tabnine keyfreq grip-mode snails benchmark-init emacsql emacsql-sqlite)
+   dotspacemacs-additional-packages '(sicp ssh-agency anki-editor company-tabnine keyfreq grip-mode snails benchmark-init emacsql emacsql-sqlite doom-themes)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -270,7 +270,8 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(doom-one
+                         spacemacs-dark
                          solarized-light
                          solarized-dark)
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
@@ -287,7 +288,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 16
+                               :size 18
                                :weight normal
                                :width normal)
 
