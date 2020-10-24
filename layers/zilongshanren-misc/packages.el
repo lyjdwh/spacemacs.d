6df7c67fccc5320e5efaa7510ae21a67c7a8e857
lyjdwh <1412511544@qq.com>
2020-10-24 09:40:43 +0800

    add elfeed filter hydra

--- layers/zilongshanren-misc/packages.el
+++ layers/zilongshanren-misc/packages.el
@@ -94,7 +94,160 @@
         ;; mingus
         helm-chrome
         bufler
+        major-mode-hydra
+        elfeed
         ))
+
+(defun zilongshanren-misc/init-major-mode-hydra ()
+  (use-package major-mode-hydra))
+
+(defun zilongshanren-misc/post-init-elfeed ()
+  (progn
+  (cl-defmacro my/elfeed-search-view-hydra-define (name body views)
+    "Define a pretty hydra named NAME with BODY and VIEWS.
+  VIEWS is a plist: in it, each property is a string which becomes
+  a column header in the hydra, and each value is a list of lists
+  in this format: (KEY COMPONENT &optional LABEL).
+
+  The KEY is a key sequence passed to `kbd', like \"s\" or \"S
+  TAB\".  The COMPONENT is an Elfeed filter component, which may
+  begin with \"+\" or \"=\", and in which spaces are automatically
+  escaped as required by Elfeed.  The LABEL, if present, is a
+  string displayed next to the KEY; if absent, COMPONENT is
+  displayed.
+
+  In the resulting hydra, when KEY is pressed, the COMPONENT is
+  toggled in `elfeed-search-filter'.  It is toggled between three
+  states: normal, inverse, and absent.  For example, the component
+  \"+tag\" cycles between three states in the filter: \"+tag\",
+  \"-tag\", and \"\".  The appropriate inverse prefix is used
+  according to the component's prefix (i.e. for \"=\", the inverse
+  is \"~\", and for \"\" (a plain regexp), \"!\" is used).
+
+  These special components may be used to read choices from the
+  Elfeed database with completion and toggle them:
+
+    :complete-age   Completes and sets the age token.
+    :complete-feed  Completes and toggles a feed token.
+    :complete-tag   Completes and toggles a tag token.
+    nil             Sets default filter.
+
+  A complete example:
+
+    (my/elfeed-search-view-hydra-define my/elfeed-search-view-hydra
+      (:foreign-keys warn)
+      (\"Views\"
+       ((\"@\" :complete-age \"Date\")
+        (\"d\" nil))
+       \"Status\"
+       ((\"su\" \"+unread\"))
+       \"Feed\"
+       ((\"f TAB\" :complete-feed \"Choose\")
+        (\"fE\" \"=Planet Emacslife\" \"Planet Emacslife\"))
+       \"Tags\"
+       ((\"t TAB\" :complete-tag \"Choose\")
+        (\"te\" \"+Emacs\"))
+       \"\"
+       ((\"tn\" \"+news\"))))"
+    (declare (indent defun))
+    (cl-labels ((escape-spaces (string)
+                               ;; Return STRING with spaces escaped with "\s-".  Necessary
+                               ;; because Elfeed treats all literal spaces as separating tokens.
+                               (replace-regexp-in-string (rx space) "\\s-" string t t)))
+      (let* ((completion-fns
+              (list (cons :complete-age
+                          (lambda ()
+                            (interactive)
+                            (save-match-data
+                              (let* ((date-regexp (rx (group (or bos blank) "@" (1+ digit) (1+ (not blank)))))
+                                     (date-tag (when (string-match date-regexp elfeed-search-filter)
+                                                 (match-string 1 elfeed-search-filter))))
+                                (elfeed-search-set-filter
+                                 (replace-regexp-in-string date-regexp (read-string "Date: " date-tag)
+                                                           elfeed-search-filter t t))))))
+                    (cons :complete-feed
+                          '(concat "=" (replace-regexp-in-string
+                                        (rx space) "\\s-"
+                                        (->> (hash-table-values elfeed-db-feeds)
+                                             (--map (elfeed-meta it :title))
+                                             (completing-read "Feed: ")
+                                             regexp-quote) t t)))
+                    (cons :complete-tag
+                          '(concat "+" (completing-read "Tag: " (elfeed-db-get-all-tags))))))
+             (body (append '(:title elfeed-search-filter :color pink :hint t :quit-key "q")
+                           body))
+             (heads (cl-loop for (heading views) on views by #'cddr
+                             collect heading
+                             collect (cl-loop for (key component label) in views
+                                              collect
+                                              `(,key
+                                                ,(cl-typecase component
+                                                   ((and function (not null))
+                                                    ;; I don't understand why nil matches
+                                                    ;; (or lambda function), but it does,
+                                                    ;; so we have to account for it.  See
+                                                    ;; (info-lookup-symbol 'cl-typep).
+                                                    `(funcall ,component))
+                                                   (string
+                                                    `(elfeed-search-set-filter
+                                                      (my/elfeed-search-filter-toggle-component
+                                                       elfeed-search-filter ,(escape-spaces component))))
+                                                   (otherwise
+                                                    `(elfeed-search-set-filter
+                                                      ,(when component
+                                                         `(my/elfeed-search-filter-toggle-component
+                                                           elfeed-search-filter ,component)))))
+                                                ,(or label component "Default"))))))
+        ;; I am so glad I discovered `cl-sublis'.  I tried several variations of `cl-labels' and
+        ;; `cl-macrolet' and `cl-symbol-macrolet', but this is the only way that has worked.
+        (setf heads (cl-sublis completion-fns heads))
+        `(pretty-hydra-define ,name ,body
+           ,heads))))
+
+  (cl-defun my/elfeed-search-filter-toggle-component (string component)
+    "Return STRING (which should be `elfeed-search-filter') having toggled COMPONENT.
+  Tries to intelligently handle components based on their prefix:
+  +tag, =feed, regexp."
+    (save-match-data
+      (cl-labels ((toggle (component +prefix -prefix string)
+                          (let ((+pat (rx-to-string `(seq (or bos blank)
+                                                          (group ,+prefix ,component)
+                                                          (or eos blank))))
+                                (-pat (rx-to-string `(seq (group (or bos (1+ blank)) ,-prefix ,component)
+                                                          (or eos blank)))))
+                            ;; TODO: In newer Emacs versions, the `rx' pattern `literal'
+                            ;; evaluates at runtime in `pcase' expressions.
+                            (pcase string
+                              ((pred (string-match +pat)) (rm (concat -prefix component) string))
+                              ((pred (string-match -pat)) (rm "" string))
+                              (_ (concat string " " +prefix component)))))
+                  (rm (new string) (replace-match new t t string 1)))
+        (pcase component
+          ((rx bos "+" (group (1+ anything)))
+           (toggle (match-string 1 component) "+" "-" string))
+          ((rx bos "=" (group (1+ anything)))
+           (toggle (match-string 1 component) "=" "~" string))
+          (_ (toggle component "" "!" string))))))
+
+  (my/elfeed-search-view-hydra-define my/elfeed-search-view-hydra
+                                              (:foreign-keys warn)
+                                              ("Views"
+                                               (("@" :complete-age "Date")
+                                                ("d" nil))
+                                               "Status"
+                                               (("wu" "+unread"))
+                                               "Feed"
+                                               (("f TAB" :complete-feed "Choose"))
+                                               "Tags"
+                                               (("t TAB" :complete-tag "Choose"))
+                                               ))
+
+  (evilified-state-evilify-map elfeed-search-mode-map
+    :mode elfeed-search-mode
+    :eval-after-load elfeed-search
+    :bindings
+    "?"  'my/elfeed-search-view-hydra/body)
+  ))
 
 (defun zilongshanren-misc/init-bufler ()
   (use-package bufler))
@@ -1366,56 +1519,6 @@ Search for a search tool in the order provided by `dotspacemacs-search-tools'."
       (spacemacs/set-leader-keys (kbd "mhm") 'discover-my-major)
       (evilified-state-evilify makey-key-mode makey-key-mode-get-key-map)
       )))
-
-
-(defun zilongshanren-misc/post-init-elfeed ()
-  (use-package elfeed
-    :init
-    (global-set-key (kbd "C-x w") 'elfeed)
-    :defer t
-    :config
-    (progn
-
-      (setq elfeed-feeds
-            '("http://nullprogram.com/feed/"
-              "http://z.caudate.me/rss/"
-              "http://irreal.org/blog/?feed=rss2"
-              "http://feeds.feedburner.com/LostInTheTriangles"
-              "http://tonybai.com/feed/"
-              "http://planet.emacsen.org/atom.xml"
-              "http://feeds.feedburner.com/emacsblog"
-              "http://blog.binchen.org/rss.xml"
-              "http://oremacs.com/atom.xml"
-              "http://blog.gemserk.com/feed/"
-              "http://www.masteringemacs.org/feed/"
-              "http://t-machine.org/index.php/feed/"
-              "http://gameenginebook.blogspot.com/feeds/posts/default"
-              "http://feeds.feedburner.com/ruanyifeng"
-              "http://coolshell.cn/feed"
-              "http://blog.devtang.com/atom.xml"
-              "http://emacsist.com/rss"
-              "http://puntoblogspot.blogspot.com/feeds/2507074905876002529/comments/default"
-              "http://angelic-sedition.github.io/atom.xml"))
-
-      ;; (evilify elfeed-search-mode elfeed-search-mode-map)
-      (evilified-state-evilify-map elfeed-search-mode-map
-        :mode elfeed-search-mode
-        :bindings
-        "G" 'elfeed-update
-        "g" 'elfeed-search-update--force)
-
-      (defun zilong/elfeed-mark-all-as-read ()
-        (interactive)
-        (mark-whole-buffer)
-        (elfeed-search-untag-all-unread))
-
-      (define-key elfeed-search-mode-map (kbd "R") 'zilong/elfeed-mark-all-as-read)
-
-      (defadvice elfeed-show-yank (after elfeed-show-yank-to-kill-ring activate compile)
-        "Insert the yanked text from x-selection to kill ring"
-        (kill-new (x-get-selection)))
-
-      (ad-activate 'elfeed-show-yank))))
 
 (defun zilongshanren-misc/post-init-evil ()
   (progn
